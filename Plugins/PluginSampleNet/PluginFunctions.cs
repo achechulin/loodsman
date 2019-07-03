035f1f442530573684f901afdb7db613c317cfd9
achechulin <achechulin@gmail.com>
2019-07-03 13:32:06 +0500

    Функция для автооперации Workflow

--- Plugins/PluginSampleNet/PluginFunctions.cs
+++ Plugins/PluginSampleNet/PluginFunctions.cs
@@ -7,9 +7,92 @@ using System.Reflection;
 using RGiesecke.DllExport;
 using Loodsman;
 using DataProvider;
+using WorkflowBusinessLogic;
 
 namespace PluginSampleNet
 {
+    // Структура TPDMVersionData для автоопераций Workflow
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public struct PDMVersionData
+    {
+        [MarshalAs(UnmanagedType.I4)]
+        public int routeId;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 255)]
+        public string appServer;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 255)]
+        public string db;
+        [MarshalAs(UnmanagedType.I4)]
+        public int id;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 255)]
+        public string product;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 255)]
+        public string type;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 255)]
+        public string version;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 255)]
+        public string state;
+        [MarshalAs(UnmanagedType.I1)]
+        public byte accessLevel;
+        [MarshalAs(UnmanagedType.I1)]
+        public byte lockLevel;
+        [MarshalAs(UnmanagedType.I1)]
+        public byte document;
+        [MarshalAs(UnmanagedType.I1)]
+        public byte revision;
+    }
+
+    // Структура Variant для автоопераций Workflow
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Variant
+    {
+        public ushort vt;
+        public ushort wReserved1;
+        public ushort wReserved2;
+        public ushort wReserved3;
+        public IntPtr data1;
+        public IntPtr data2;
+
+        public Variant(VarEnum type)
+        {
+            vt = (ushort)type;
+            wReserved1 = 0;
+            wReserved2 = 0;
+            wReserved3 = 0;
+            data1 = (IntPtr)0;
+            data2 = (IntPtr)0;
+        }
+
+        public Variant(bool value)
+        {
+            vt = (ushort)VarEnum.VT_BOOL;
+            wReserved1 = 0;
+            wReserved2 = 0;
+            wReserved3 = 0;
+            data1 = (IntPtr)Convert.ToInt32(value);
+            data2 = (IntPtr)0;
+        }
+
+        public Variant(int value)
+        {
+            vt = (ushort)VarEnum.VT_I4;
+            wReserved1 = 0;
+            wReserved2 = 0;
+            wReserved3 = 0;
+            data1 = (IntPtr)value;
+            data2 = (IntPtr)0;
+        }
+
+        public Variant(string value)
+        {
+            vt = (ushort)VarEnum.VT_BSTR;
+            wReserved1 = 0;
+            wReserved2 = 0;
+            wReserved3 = 0;
+            data1 = (IntPtr)UnsafeNativeMethods.SysAllocString(value);
+            data2 = (IntPtr)0;
+        }
+    }
+
     public class PluginFunctions
     {
         private static bool _initialized = false;
@@ -58,6 +141,11 @@ namespace PluginSampleNet
             UnsafeNativeMethods.SetWindowLong(form.Handle, UnsafeNativeMethods.GWL_HWNDPARENT, (IntPtr)pluginCall.ClientHandle);
             form.PluginCall = pluginCall;
             form.ShowDialog();
+        }
+
+        public static bool WFProjectList(IWFBusinessLogic wf, PDMVersionData versionData, object[] userData)
+        {
+            return true;
         }
 
         // Загрузка используемых сборок из папки, в которой находится текущая сборка.
@@ -155,6 +243,33 @@ namespace PluginSampleNet
             catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString(), PluginCaption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Функция для использования в автооперации Workflow с помощью ExecPluginFunction 
+        [DllExport("WFProjectList", CallingConvention.StdCall)]
+        public static Variant _wfProjectList(Variant wfo, IntPtr versionData, object[] userData)
+        {
+            if (!_initialized)
+            {
+                Initialize();
+                _initialized = true;
+            }
+            try
+            {
+                if (wfo.vt != (ushort)VarEnum.VT_DISPATCH) 
+                { 
+                    throw new ArgumentException();
+                }
+                IWFBusinessLogic wf = (IWFBusinessLogic)Marshal.GetTypedObjectForIUnknown(wfo.data1, typeof(IWFBusinessLogic));
+                PDMVersionData data = (PDMVersionData)Marshal.PtrToStructure(versionData, typeof(PDMVersionData));
+                bool res = WFProjectList(wf, data, userData);
+                return new Variant(res);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), PluginCaption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return new Variant(VarEnum.VT_NULL);
             }
         }
     }
--- Plugins/PluginSampleNet/UnsafeNativeMethods.cs
+++ Plugins/PluginSampleNet/UnsafeNativeMethods.cs
@@ -5,7 +5,13 @@ namespace PluginSampleNet
 {
     internal static class UnsafeNativeMethods
     {
+        // SetWindowLong
         public const int GWL_HWNDPARENT = -8;
+        // _controlfp
+        // Default Delphi FPU control word
+        public const int Default8087CW = 0x00001332;
+        // Default CLR FPU control word
+        public const int _CW_DEFAULT = 0x0009001F;
         //SetWindowLong won't work correctly for 64-bit: we should use SetWindowLongPtr instead.  On
         //32-bit, SetWindowLongPtr is just #defined as SetWindowLong.  SetWindowLong really should 
         //take/return int instead of IntPtr/HandleRef, but since we're running this only for 32-bit
@@ -22,5 +28,9 @@ namespace PluginSampleNet
         public static extern IntPtr SetWindowLongPtr32(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
         [DllImport("user32.dll", CharSet = CharSet.Auto, EntryPoint = "SetWindowLongPtr")]
         public static extern IntPtr SetWindowLongPtr64(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+        [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern UInt32 _controlfp(UInt32 newControl, UInt32 mask);
+        [DllImport("oleaut32.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern IntPtr SysAllocString([MarshalAs(UnmanagedType.LPWStr)] string sz);
     }
 }
